891214cd41727cd6288e5a63477f9de32fd06133
monw <65690955+monw@users.noreply.github.com>
2022-11-24 12:14:48 +0800

    Update zerotier.sh

--- trunk/user/zerotier/zerotier.sh
+++ trunk/user/zerotier/zerotier.sh
@@ -104,7 +104,7 @@ rules() {
 	iptables -A FORWARD -i $zt0 -j ACCEPT
 	if [ $nat_enable -eq 1 ]; then
 		iptables -t nat -A POSTROUTING -o $zt0 -j MASQUERADE
-		ip_segment="$(ip route | grep "dev $zt0 proto kernel" | awk '{print $1}')"
+		ip_segment=$(ip route | grep "dev $zt0  proto kernel" | awk '{print $1}')
 		iptables -t nat -A POSTROUTING -s $ip_segment -j MASQUERADE
 		zero_route "add"
 	fi
