945effb5b2d82a8638e02b7ad4f175897848cd0a
fightroad <39027157+fightroad@users.noreply.github.com>
2023-12-06 09:58:17 +0800

    Update variables.c

--- trunk/user/httpd/variables.c
+++ trunk/user/httpd/variables.c
@@ -1006,6 +1006,7 @@
 			{"zerotier_enable", "", NULL, EVM_RESTART_ZEROTIER},
 			{"zerotier_id", "", NULL, EVM_RESTART_ZEROTIER},
 			{"zerotier_moonid", "", NULL, EVM_RESTART_ZEROTIER},
+                        {"zerotier_planet", "", NULL, EVM_RESTART_ZEROTIER},
 			{"zerotier_nat", "", NULL, EVM_RESTART_ZEROTIER},
 			{"zero_staticnum_x", "", NULL, EVM_RESTART_ZEROTIER},
 			{"ZeroList", "Group", ARGV((char*)variables_ZeroConf_ZeroList, "8", "55", "zero_staticnum_x"), EVM_RESTART_ZEROTIER},
